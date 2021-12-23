d1fe4e10c7cc1aa49755a7f19c3848c13d0355e0
Anurag Bansal <bansal.anurag568@gmail.com>
2021-12-23 16:23:26 +0530

    Updates

---
+++ precache-manifest.a8c51e5daa7bced3c9a875c984c5010b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f17f6dbd567a1ad1ea82280f7d66c4f",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "34bab6b2ecd5d750c778",
+    "url": "/react-calculator/static/css/main.541d905d.chunk.css"
+  },
+  {
+    "revision": "804afeebc4119098940f",
+    "url": "/react-calculator/static/js/2.cd25812d.chunk.js"
+  },
+  {
+    "revision": "34bab6b2ecd5d750c778",
+    "url": "/react-calculator/static/js/main.2387f9cf.chunk.js"
+  },
+  {
+    "revision": "e546b3e323215319a407",
+    "url": "/react-calculator/static/js/runtime~main.00b8a6fe.js"
+  }
+]);
