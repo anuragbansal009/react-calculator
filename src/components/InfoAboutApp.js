60e068ae82dfd6eb1881d34fb9bf2df5d9e3e63b
Anurag Bansal <bansal.anurag568@gmail.com>
2021-12-23 16:24:17 +0530

    Adding Files

---
+++ src/components/InfoAboutApp.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class InfoAboutApp extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div>
+          <h1 className="App-title">React Calculator</h1>
+        </div>
+        <br />
+      </React.Fragment>
+  )}
+}
+
+export default InfoAboutApp;
